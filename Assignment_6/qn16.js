99a637c6afb2dad237ff1bd11a6326208b841684
Nikhil singh <nikhilkumarofficial770@gmail.com>
2025-02-04 21:33:09 +0530

    Create qn16.js

---
+++ Assignment_6/qn16.js
@@ -0,0 +1,44 @@
+function ascending(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[i] > arr[j]) {
+        let temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+      }
+    }
+  }
+  return arr;
+}
+
+console.log(ascending([5, 2, 9, 1, 5, 6])); 
+
+function ascending(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[i] > arr[j]) {
+        let temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+      }
+    }
+  }
+  return arr;
+}
+
+function descending(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[i] < arr[j]) {
+        let temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+      }
+    }
+  }
+  return arr;
+}
+
+console.log(ascending([5, 2, 9, 1, 5, 6])); 
+console.log(descending([5, 2, 9, 1, 5, 6]));
+
